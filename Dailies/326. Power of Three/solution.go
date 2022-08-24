f0787e5a19a5cd6c80cd559507fe864caa974e1b
Zack <zack@zacklott.co.uk>
2022-08-24 21:24:02 +0100

    Dailies - Power of Three

---
+++ Dailies/326. Power of Three/solution.go
@@ -0,0 +1,15 @@
+func isPowerOfThree(n int) bool {
+    if n <= 0 {
+        return false
+    }
+    
+    if n % 3 == 0 {
+        return isPowerOfThree(n / 3)
+    }
+    
+    if n == 1 {
+        return true
+    }
+    
+    return false
+}
