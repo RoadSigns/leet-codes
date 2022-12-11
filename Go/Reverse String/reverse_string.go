adaa6a5ee96be9ad0e1dc9395a70faa9f6255b79
Zack <zack@zacklott.co.uk>
2022-12-11 20:18:43 +0000

    Reserve String by Pointer

--- Go/Reverse String/reverse_string.go
+++ Go/Reverse String/reverse_string.go
@@ -7,3 +7,9 @@ func ReverseString(s []byte) []byte {
 	}
 	return b
 }
+
+func ReverseStringByPointer(s []byte) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
