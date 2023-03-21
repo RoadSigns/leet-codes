7d5530b824f15e5659c8a59303a908b7f2aabb87
Zack <zack@zacklott.co.uk>
2023-03-21 21:16:30 +0000

    Prefix total

--- Go/Minimum Value to Get Positive Step by Step Sum/minimum_value_to_get_positive_step_by_step_sum.go
+++ Go/Minimum Value to Get Positive Step by Step Sum/minimum_value_to_get_positive_step_by_step_sum.go
@@ -1,19 +1,14 @@
 package minimum_value_to_get_positive_step_by_step_sum
 
+import "math"
+
 func MinStartValue(nums []int) int {
-	current := 1
-	for {
-		tmp := current
-		for _, i := range nums {
-			tmp += i
-			if tmp < 1 {
-				current++
-				break
-			}
-		}
-		if tmp < 1 {
-			continue
-		}
-		return current
+	var minVal int
+	var total int
+
+	for i := 0; i < len(nums); i++ {
+		total += nums[i]
+		minVal = int(math.Min(float64(minVal), float64(total)))
 	}
+	return -minVal + 1
 }
