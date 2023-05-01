ff4eb759ac79d8a3835527784ddbed2402521ffc
Zack <zack@zacklott.co.uk>
2023-05-01 16:40:04 +0100

    Invert Binary Tree

---
+++ Go/Binary Search/invert_binary_tree.go
@@ -0,0 +1,42 @@
+package binary_tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func InvertTree(root *TreeNode) *TreeNode {
+	var l *TreeNode
+	var r *TreeNode
+
+	if root == nil {
+		return root
+	}
+
+	if root.Left != nil {
+		l = InvertTree(root.Left)
+	}
+
+	if root.Right != nil {
+		r = InvertTree(root.Right)
+	}
+
+	return &TreeNode{
+		Val:   root.Val,
+		Left:  r,
+		Right: l,
+	}
+}
+
+func InvertTreeRefactored(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	root.Left, root.Right = root.Right, root.Left
+	InvertTree(root.Left)
+	InvertTree(root.Right)
+
+	return root
+}
